34157f4c4a169d0222e5efae24f307e8c0169ddf
Borja <borjamese@gmail.com>
2023-04-04 15:02:33 +0000

    contador ready

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -2,12 +2,19 @@ import React from "react"
 import "../../styles/index.css";
 
 export default function SecondsCounter ({seconds}) {
-return (
-<div className = "App">
-
-<p>{seconds}</p>
-
-</div>
-);
-
-}
+        const secondsString = seconds.toString().padStart(5, '0');
+        const secondsArray = secondsString.split('');
+      
+        return (
+        <div className="App">
+            
+          <div className="counter">
+            {secondsArray.map((digit, index) => (
+              <span key={index} className="counter__number">{digit}</span>
+            ))}
+            <span className="counter__label">Seconds</span>
+          </div>
+          
+        </div>
+        );
+      }
